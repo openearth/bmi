d8ccbe1b98214064684a94293baae7cbd7334eab
Fedor Baart <fedor.baart@deltares.nl>
2014-07-14 17:23:25 +0200

    implement set_var

--- models/fortran/model.f90
+++ models/fortran/model.f90
@@ -13,9 +13,9 @@ module demo_model
   double precision, target :: t_end
   double precision, target :: t_start
 
-  integer, target :: var1
-  double precision, target :: var2(2)
-  logical(c_bool), target :: var3(2,3)
+  double precision, target :: arr1(3)
+  integer, target :: arr2(2,3)
+  logical(c_bool), target :: arr3(2,2,3)
 contains
 
 
@@ -45,6 +45,7 @@ contains
     write(msgbuf,*) 'Initializing with ', configfile
     call log(LEVEL_INFO, trim(msgbuf))
 
+    arr1 = (/3,  2, 1/)
   end function initialize
 
 
@@ -81,11 +82,11 @@ contains
     var_name = char_array_to_string(c_var_name)
 
     select case(var_name)
-    case('var1')
+    case('arr1')
        type_name = 'double'
-    case('var2')
+    case('arr2')
        type_name = 'int'
-    case('var3')
+    case('arr3')
        type_name = 'bool'
     case default
     end select
@@ -106,12 +107,12 @@ contains
     var_name = char_array_to_string(c_var_name)
 
     select case(var_name)
-    case("var1")
-       rank = 0
-    case("var2")
+    case("arr1")
        rank = 1
-    case("var3")
+    case("arr2")
        rank = 2
+    case("arr3")
+       rank = 3
     case default
        rank = 0
     end select
@@ -129,11 +130,12 @@ contains
     shape = (/0, 0, 0, 0, 0, 0/)
 
     select case(var_name)
-    case("var1")
-    case("var2")
-       shape(1:1) = 2
-    case("var3")
+    case("arr1")
+       shape(1:1) = 3
+    case("arr2")
        shape(1:2) = (/2, 3/)
+    case("arr3")
+       shape(1:3) = (/2, 2, 3/)
     end select
   end subroutine get_var_shape
 
@@ -152,16 +154,56 @@ contains
     var_name = char_array_to_string(c_var_name)
 
     select case(var_name)
-    case("var1")
-       x = c_loc(var1)
-    case("var2")
-       x = c_loc(var2)
-    case("var3")
-       x = c_loc(var3)
+    case("arr1")
+       x = c_loc(arr1)
+    case("arr2")
+       x = c_loc(arr2)
+    case("arr3")
+       x = c_loc(arr3)
     end select
 
   end subroutine get_var
 
+subroutine set_var(c_var_name, xptr) bind(C, name="set_var")
+  !DEC$ ATTRIBUTES DLLEXPORT :: set_var
+  ! Return a pointer to the variable
+  use iso_c_binding, only: c_double, c_char, c_loc, c_f_pointer
+
+  character(kind=c_char), intent(in) :: c_var_name(*)
+  type(c_ptr), value, intent(in) :: xptr
+
+
+  real(c_double), pointer :: x_1d_double_ptr(:)
+  real(c_double), pointer :: x_2d_double_ptr(:,:)
+  real(c_double), pointer :: x_3d_double_ptr(:,:,:)
+  integer(c_int), pointer :: x_1d_int_ptr(:)
+  integer(c_int), pointer :: x_2d_int_ptr(:,:)
+  integer(c_int), pointer :: x_3d_int_ptr(:,:,:)
+  real(c_float), pointer  :: x_1d_float_ptr(:)
+  real(c_float), pointer  :: x_2d_float_ptr(:,:)
+  real(c_float), pointer  :: x_3d_float_ptr(:,:,:)
+  logical(c_bool), pointer  :: x_1d_bool_ptr(:)
+  logical(c_bool), pointer  :: x_2d_bool_ptr(:,:)
+  logical(c_bool), pointer  :: x_3d_bool_ptr(:,:,:)
+
+  ! The fortran name of the attribute name
+  character(len=strlen(c_var_name)) :: var_name
+  ! Store the name
+  var_name = char_array_to_string(c_var_name)
+
+  select case(var_name)
+  case("arr1")
+     call c_f_pointer(xptr, x_1d_double_ptr, shape(arr1))
+     arr1(:) = x_1d_double_ptr
+  case("arr2")
+     call c_f_pointer(xptr, x_2d_int_ptr, shape(arr2))
+     arr2(:,:) = x_2d_int_ptr
+  case("arr3")
+     call c_f_pointer(xptr, x_3d_bool_ptr, shape(arr3))
+     arr3(:,:,:) = x_3d_bool_ptr
+  end select
+
+end subroutine set_var
   subroutine get_current_time(time) bind(C, name="get_current_time")
     !DEC$ ATTRIBUTES DLLEXPORT :: get_current_time
 
